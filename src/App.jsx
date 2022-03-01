97c4b727e80804411280c3fc42756831efbf1f61
Kicksquare <90344282+nf-projects@users.noreply.github.com>
2022-03-01 01:47:12 +0000

    changed favicon and text

--- src/App.jsx
+++ src/App.jsx
@@ -76,9 +76,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header gradient-text">My NFT Collection</p>
+          <p className="header gradient-text">NFT Collection Minter</p>
           <p className="sub-text">
-            Each unique. Each beautiful. Discover your NFT today.
+            Minter-dApp to mint randomly generated phrases as NFTs.
           </p>
           {currentAccount === "" ? (
             renderNotConnectedContainer()
